90cfbde536f7e8484b798f4c8aab3e51e1308753
zetos <cesar_manoel@hotmail.com>
2026-07-19 17:33:53 -0300

    chore: remove unused assets and configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,11 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
   subsets: ["latin"],
 });
 
@@ -39,7 +34,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} ${abbess.variable} ${visit.variable} antialiased`}
+        className={`${geistSans.variable} ${abbess.variable} ${visit.variable} antialiased`}
       >
         <Header />
         {children}
